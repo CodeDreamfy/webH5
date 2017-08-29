7ee0ac222336ab429d4f290971723b0c00974d3b
CodeDreamfy <codedreamfy@outlook.com>
2017-08-29 20:10:26 +0800

    去重

--- src/components/history.vue
+++ src/components/history.vue
@@ -27,22 +27,46 @@ export default {
   methods: {
     insertDom(data) {
       const docFragment = document.createDocumentFragment();
-      const elemDivWrap = document.createElement('<div class="datalist"><ul></ul></div>');
+      const elemDivWrap = document.createElement('div');
+      elemDivWrap.className = 'datalist';
+      elemDivWrap.innerHTML = '<ul></ul>';
       const ul = elemDivWrap.querySelector('ul');
-      for (let i = 0; i < data.length; i++) {
-        const date = new Date(data[i].at);
+      console.log('insertData', data);
+      if (data.length === 0) {
         const li = document.createElement(`
-        <li>用户ID:${data[i].Uid}在${date.getHours()}:${date.getMinutes()}</li>`);
-        ul.insertAdjacentElement(li);
+        <li>暂无数据</li>`);
+        ul.appendChild(li);
+      } else {
+        for (let i = 0; i < data.length; i += 1) {
+          const date = new Date(data[i].at);
+          const li = document.createElement('li');
+          li.innerHTML = `
+        <li>用户ID:${data[i].Uid}在${date.getHours()}:${date.getMinutes()}打开门磁ID为${data[i].Did}的门</li>`;
+          ul.appendChild(li);
+        }
       }
+
+      docFragment.appendChild(elemDivWrap);
+      const wrap = document.getElementsByTagName('section')[0];
+      wrap.appendChild(docFragment);
+    },
+    clearDom() {
+      const wrap = document.getElementsByTagName('section')[0];
+      wrap.innerHTML = '';
     },
   },
   mounted() { // eslint-disable-next-line
     const that = this;
-    const sensorPromise = {};
-    let senData;
+    const sensorPromise = this.getSensordata();
     sensorPromise.then((res) => {
-      senData = this.getDataFormat(res);
+      this.clearDom();
+      const ms = new Promise((resolve) => {
+        const da = this.getDataFormat(res.data.data);
+        resolve(da);
+      });
+      ms.then((data) => {
+        this.insertDom(data);
+      });
     });
   },
 };
--- src/utils.js
+++ src/utils.js
@@ -8,27 +8,24 @@ utils.install = () => {
     methods: {
       getToken() {
         const that = this;
-        const oToken = JSON.parse(localStorage.getItem('otoken'));
+        const oToken = (localStorage.getItem('otoken') && JSON.parse(localStorage.getItem('otoken'))) || null;
         let ntime = +new Date();
         return new Promise((resolve, reject) => {
-          if (oToken && ntime < oToken.time) {
-            resolve(oToken.id);
+          if (oToken && ntime < oToken.etime) {
+            resolve(oToken.token);
           } else {
             that.$axios({
               method: 'get',
-              url: 'http://api.heclouds.com/pp/token?user_id=23709&secret=y937q50ElKG8lwfkazA7OOwaRu9Up0YB',
+              url: '//api.heclouds.com/pp/token?user_id=23709&secret=y937q50ElKG8lwfkazA7OOwaRu9Up0YB',
               data: {},
             })
             .then((res) => {  // eslint-disable-next-line
-              if (oToken.id) {
-                localStorage.removeItem('otoken');
-              }
               ntime = +new Date();
-              localStorage.setItem('otoken', String.stringify({
-                token: res.token,
-                etime: ntime + (24 * 3600000),
+              localStorage.setItem('otoken', JSON.stringify({
+                token: res.data.data.token,
+                etime: ntime + 2333000,
               }));
-              resolve(res.token);
+              resolve(res.data.data.token);
             })
             .catch((err) => { // eslint-disable-next-line
               reject(err);
@@ -40,28 +37,31 @@ utils.install = () => {
         const that = this;
         const tokenPromise = this.getToken();
         let oArg = {};
-        tokenPromise.then((res) => {
-          oArg = Object.assign({
-            token: res,
-            stime: new Date(),
-            etime: +new Date() + (24 * 3600000),
-            page: 1,
-            limit: 12,
-          }, ...args);
-        }).then((err) => {
-          throw err;
-        });
+        let argument = args;
         return new Promise((resolve, reject) => {
-          that.$axios({
-            method: 'get',
-            url: `http://api.heclouds.com/pp/device/${OJS.device.id}/?token=${oArg.token}&product_id=${92352}&begin=${oArg.stime}&end=${oArg.etime}&page=${oArg.page}&pre_page=${oArg.limit}`,
-            data: {},
-          })
-          .then((res) => {  // eslint-disable-next-line
-            resolve(res);
-          })
-          .catch((err) => { // eslint-disable-next-line
-            reject(err);
+          tokenPromise.then((res) => {
+            if (!argument) { argument = {}; }
+            oArg = Object.assign({
+              token: res,
+              stime: new Date().toISOString(),
+              etime: new Date(+new Date() + (24 * 3600000)).toISOString(),
+              page: 1,
+              limit: 12,
+              proid: 92352,
+            }, args);
+            that.$axios({
+              method: 'get',
+              url: `//api.heclouds.com/pp/device/${OJS.device.id}/sensordata?token=${oArg.token}&product_id=${oArg.proid}&begin=${oArg.stime}&end=${oArg.etime}&page=${oArg.page}&pre_page=${oArg.limit}`,
+              data: {},
+            })
+            .then((datas) => {  // eslint-disable-next-line
+              resolve(datas);
+            })
+            .catch((err) => { // eslint-disable-next-line
+              reject(err);
+            });
+          }).then((errs) => {
+            console.log(errs);
           });
         });
       },
@@ -73,9 +73,19 @@ utils.install = () => {
         for (let i = 0; i < data.items.length; i += 1) {
           const d = data.items[i];
           datas[i] = Object.assign({
-            time: d.at,
+            at: d.at,
           }, d.body);
         }
+        let firstdata = +new Date(datas[0].at);
+        datas.reduce((acc, curr, index) => {
+          const n = +new Date(curr.at);
+          if (n - firstdata < 6000) {
+            datas.splice(index, 1);
+          } else {
+            firstdata = new Date(curr.at);
+          }
+          return curr;
+        });
         return datas;
       },
     },
